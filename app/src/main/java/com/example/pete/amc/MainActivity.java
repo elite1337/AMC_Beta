8ec47de15af32bb27797a82b31d930de3d077f2c
elite1337 <peterson0210@yahoo.com.tw>
2016-07-21 19:04:59 +0800

    shit ton of stuffs integrated

--- app/src/main/java/com/example/pete/amc/MainActivity.java
+++ app/src/main/java/com/example/pete/amc/MainActivity.java
@@ -34,7 +34,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         this.a = a;
     }
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
